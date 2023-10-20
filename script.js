6b24ee0ff2761f206629063b517e0414f385daed
2122k2954 <148541241+2122k2954@users.noreply.github.com>
2023-10-20 15:42:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+let homes = document.getElementById('.home');
+console.log(homes);
+
+let abouts = document.getElementById('.about');
+console.log(abouts);
+
+let departments = document.getElementById('.department');
+console.log(departments);
+
+let courses = document.getElementById('.course');
+console.log(courses);
+
+let contacts = document.getElementById('.contact');
+console.log(contacts);
+
+
+
+homes.addEventListener('click',()=>{
+  console.log(result("hom-pic"));
+});
+
+
+homes.addEventListener('click',()=>{
+  console.log(result("hom-pic"));
+});
+
+
+homes.addEventListener('click',()=>{
+  console.log(result("hom-pic"));
+});
+
+
+homes.addEventListener('click',()=>{
+  console.log(result("hom-pic"));
+});
+
+
+homes.addEventListener('click',()=>{
+  console.log(result("hom-pic"));
+});
+
+
+// Wait for the document to load
+document.addEventListener("DOMContentLoaded", function () {
+    // Get references to the button and result area
+    var homeButton = document.getElementById("hom-pic");
+    var resultArea = document.getElementById("clg-img");
+
+    // Define a function to show the result
+    function showResult() {
+        // You can replace this with your own result logic
+        // var resultText = homes.home/
+        
+        // Update the content of the result area
+        resultArea.textContent;
+    }
+
+    // Add a click event listener to the button
+    homeButton.addEventListener("click", showResult);
+});
+
+
+
+// Wait for the document to load
+document.addEventListener("DOMContentLoaded", function () {
+    // Get references to the button and result area
+    var aboutButton = document.getElementById("abts");
+    var resultArea = document.getElementById("about");
+
+    // Define a function to show the result
+    function showResult() {
+        // You can replace this with your own result logic
+        // var resultText = homes.home
+        
+        // Update the content of the result area
+        resultArea.textContent;
+    }
+
+    // Add a click event listener to the button
+    aboutButton.addEventListener("click", showResult);
+});
+
